01cc1c4fe2d335ff1e35fb95a374134406d0b8e7
sheep@atos.wmid.amu.edu.pl <sheep@atos.wmid.amu.edu.pl>
2009-08-14 16:30:37 +0200

    add widnows icon

--- setup.py
+++ setup.py
@@ -78,6 +78,64 @@ if platform == 'darwin':
     config['setup_requires'] = ['py2app']
 elif platform == 'win32':
     ### Windows installer ###
+    import py2exe
+    class InnoScript(object):
+        def __init__(self, name, lib_dir, dist_dir, windows_exe_files = [],
+                     lib_files = [], version = "1.0"):
+            self.lib_dir = lib_dir
+            self.dist_dir = dist_dir
+            if not self.dist_dir[-1] in "\\/":
+                self.dist_dir += "\\"
+            self.name = name
+            self.version = version
+            self.windows_exe_files = [self.chop(p) for p in windows_exe_files]
+            self.lib_files = [self.chop(p) for p in lib_files]
+
+        def chop(self, pathname):
+            assert pathname.startswith(self.dist_dir)
+            return pathname[len(self.dist_dir):]
+        
+        def create(self, pathname="dist\\hatta.iss"):
+            self.pathname = pathname
+            ofi = self.file = open(pathname, "w")
+            print >> ofi, "; WARNING: This script has been created by py2exe. Changes to this script"
+            print >> ofi, "; will be overwritten the next time py2exe is run!"
+            print >> ofi, r"[Setup]"
+            print >> ofi, r"AppName=%s" % self.name
+            print >> ofi, r"AppVerName=%s %s" % (self.name, self.version)
+            print >> ofi, r"DefaultDirName={pf}\%s" % self.name
+            print >> ofi, r"DefaultGroupName=%s" % self.name
+            print >> ofi
+
+            print >> ofi, r"[Files]"
+            for path in self.windows_exe_files + self.lib_files:
+                print >> ofi, r'Source: "%s"; DestDir: "{app}\%s"; Flags: ignoreversion' % (path, os.path.dirname(path))
+            print >> ofi
+
+            print >> ofi, r"[Icons]"
+            for path in self.windows_exe_files:
+                print >> ofi, r'Name: "{group}\%s"; Filename: "{app}\%s"' % \
+                      (self.name, path)
+            print >> ofi, 'Name: "{group}\Uninstall %s"; Filename: "{uninstallexe}"' % self.name
+
+        def compile(self):
+            try:
+                import ctypes
+            except ImportError:
+                try:
+                    import win32api
+                except ImportError:
+                    import os
+                    os.startfile(self.pathname)
+                else:
+                    print "Ok, using win32api."
+                    win32api.ShellExecute(0, "compile", self.pathname, None, None, 0)
+            else:
+                print "Cool, you have ctypes installed."
+                res = ctypes.windll.shell32.ShellExecuteA(0, "compile", self.pathname, None, None, 0)
+                if res < 32:
+                    raise RuntimeError, "ShellExecute failed, error %d" % res
+
     class BuildInstaller(py2exe.build_exe.py2exe):
         """
            This class first builds the exe file(s),
@@ -99,11 +157,11 @@ elif platform == 'win32':
             script.compile()
             # Note: By default the final setup.exe will be in an
             # Output subdirectory.
-    config['cmdclass'] = {"py2exe-installer": BuildInstaller}
-    config['console'] = [{
+    config['cmdclass'] = {"py2exe": BuildInstaller}
+    config['console'] = {
         'script': 'hatta.py',
         'icon_resources': [(1, "resources/hatta.ico")],
-    }],
+    },
 else: # Other UNIX-like
     config['scripts'] = ['hatta_qticon.py', 'hatta_gtkicon.py']
 

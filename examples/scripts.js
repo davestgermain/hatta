5e0272f465f4f143da59e63063a93941fe7f4c71
sheep@ghostwheel <sheep@ghostwheel>
2009-12-18 09:31:38 +0100

    fix the date js

--- examples/scripts.js
+++ examples/scripts.js
@@ -2,20 +2,13 @@ function localize_dates() {
 /* Scan whole document for UTC dates and replace them with localtime versions */
     var parse_date = function (text) {
         var m = /^([0-9]{4})-([0-9]{2})-([0-9]{2})T([0-9]{2}):([0-9]{2}):([0-9]{2})Z$/.exec(text);
-        var d = new Date();
-        d.setUTCFullYear(+m[1]);
-        d.setUTCMonth(+m[2]);
-        d.setUTCDate(+m[3]);
-        d.setUTCHours(+m[4]);
-        d.setUTCMinutes(+m[5]);
-        d.setUTCSeconds(+m[6]);
-        return d;
+        return new Date(Date.UTC(+m[1], +m[2]-1, +m[3], +m[4], +m[5], +m[6]));
     }
     var format_date = function (d) {
         var p = function(n) {return ('00'+n).slice(-2); };
         var tz = d.getTimezoneOffset()/60;
         if (tz>0) { tz = "+"+tz; }
-        return ""+d.getFullYear()+"-"+p(d.getMonth())+"-"+p(d.getDate())+" "+p(d.getHours())+":"+p(d.getMinutes())+" GMT"+tz;
+        return ""+d.getFullYear()+"-"+p(d.getMonth()+1)+"-"+p(d.getDate())+" "+p(d.getHours())+":"+p(d.getMinutes())+" GMT"+tz;
     }
     var nodes = document.getElementsByTagName('abbr');
     for (var i=0; i<nodes.length; ++i) {

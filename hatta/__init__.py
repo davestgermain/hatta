f22e8640bbc2869f896fefe640e74c97df7e2466
Radomir Dopieralski <hatta-dev@sheep.art.pl>
2010-08-09 15:04:29 +0200

    Separate the main application into separate module.

--- hatta/__init__.py
+++ hatta/__init__.py
@@ -17,63 +17,12 @@ the repository will appear in the recent changes and in page's history.
 See hatta.py --help for usage.
 """
 
-import os
-import sys
-
-# Avoid WSGI errors, see http://mercurial.selenic.com/bts/issue1095
-sys.stdout = sys.__stdout__
-sys.stderr = sys.__stderr__
-
-from wiki import Wiki, WikiResponse, WikiRequest
-from config import WikiConfig, read_config
+from hatta.wiki import Wiki, WikiResponse, WikiRequest
+from hatta.config import WikiConfig, read_config
+from hatta.__main__ import main, application
 
 __version__ = '1.4.0dev'
 project_name = 'Hatta'
 project_url = 'http://hatta-wiki.org/'
 project_description = 'Wiki engine that lives in Mercurial repository.'
 
-
-def application(env, start):
-    """Detect that we are being run as WSGI application."""
-
-    global application
-    config = read_config()
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    if config.get('pages_path') is None:
-        config.set('pages_path', os.path.join(script_dir, 'docs'))
-    wiki = Wiki(config)
-    application = wiki.application
-    return application(env, start)
-
-def main(config=None, wiki=None):
-    """Start a standalone WSGI server."""
-
-    config = config or read_config()
-    wiki = wiki or Wiki(config)
-    app = wiki.application
-
-    host, port = (config.get('interface', '0.0.0.0'),
-                  int(config.get('port', 8080)))
-    try:
-        from cherrypy import wsgiserver
-    except ImportError:
-        try:
-            from cherrypy import _cpwsgiserver as wsgiserver
-        except ImportError:
-            import wsgiref.simple_server
-            server = wsgiref.simple_server.make_server(host, port, app)
-            try:
-                server.serve_forever()
-            except KeyboardInterrupt:
-                pass
-            return
-    apps = [('', app)]
-    name = wiki.site_name
-    server = wsgiserver.CherryPyWSGIServer((host, port), apps, server_name=name)
-    try:
-        server.start()
-    except KeyboardInterrupt:
-        server.stop()
-
-if __name__ == "__main__":
-    main()
---
+++ hatta/__main__.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+# Avoid WSGI errors, see http://mercurial.selenic.com/bts/issue1095
+sys.stdout = sys.__stdout__
+sys.stderr = sys.__stderr__
+
+def application(env, start):
+    """Detect that we are being run as WSGI application."""
+
+    global application
+    config = read_config()
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    if config.get('pages_path') is None:
+        config.set('pages_path', os.path.join(script_dir, 'docs'))
+    wiki = Wiki(config)
+    application = wiki.application
+    return application(env, start)
+
+def main(config=None, wiki=None):
+    """Start a standalone WSGI server."""
+
+    config = config or read_config()
+    wiki = wiki or Wiki(config)
+    app = wiki.application
+
+    host, port = (config.get('interface', '0.0.0.0'),
+                  int(config.get('port', 8080)))
+    try:
+        from cherrypy import wsgiserver
+    except ImportError:
+        try:
+            from cherrypy import _cpwsgiserver as wsgiserver
+        except ImportError:
+            import wsgiref.simple_server
+            server = wsgiref.simple_server.make_server(host, port, app)
+            try:
+                server.serve_forever()
+            except KeyboardInterrupt:
+                pass
+            return
+    apps = [('', app)]
+    name = wiki.site_name
+    server = wsgiserver.CherryPyWSGIServer((host, port), apps, server_name=name)
+    try:
+        server.start()
+    except KeyboardInterrupt:
+        server.stop()
+
+if __name__ == "__main__":
+    main()
+

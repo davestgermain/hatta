35836663cdd8a5ed7b7aeac03b74254c24eaec5b
Radomir Dopieralski <sheep-devel@sheep.art.pl>
2013-05-18 12:21:34 +0200

    Update locale

---
+++ babel.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+pybabel extract --mapping=babel.ini --project='Hatta Wiki' --version='1.6.0' --copyright-holder='Radomir Dopieralski' --msgid-bugs-address='hatta@sheep.art.pl' --output=locale/hatta.pot hatta/

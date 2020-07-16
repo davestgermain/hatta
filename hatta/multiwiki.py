40111c6a43afabdef941bd035fe3469d7ef746d5
Dave St.Germain <dave@st.germa.in>
2020-07-15 23:26:40 -0400

    Multi-wiki can optionally mount wikis at different paths

--- hatta/multiwiki.py
+++ hatta/multiwiki.py
@@ -7,7 +7,9 @@ import os, os.path
 import glob
 
 import hatta
+import werkzeug
 from werkzeug.middleware.proxy_fix import ProxyFix
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
 
 config_dir = os.environ['HATTA_CONFIG_DIR']
@@ -16,20 +18,35 @@ DEFAULT_HOST = 'localhost'
 APPS = {}
 
 
+@werkzeug.wsgi.responder
+def default_application(environ, start_response):
+    return werkzeug.Response()
+
+
 for conf_file in glob.glob(os.path.join(config_dir, '*.conf')):
     config = hatta.WikiConfig()
     config.parse_files([conf_file])
     config.sanitize()
-    # file name is the domain name
-    domain_name = os.path.basename(os.path.splitext(conf_file)[0])
+    wsgi_app = hatta.Wiki(config).application
+
+    # file name is the domain name _path
+    # if the path is set, use the dispatcher middleware
+    splitname = os.path.basename(os.path.splitext(conf_file)[0]).split('_', 1)
+    domain_name = splitname[0]
     if DEBUG:
         import pprint
-        print(domain_name)
+        print('/'.join(splitname))
         pprint.pprint(config.config)
-    APPS[domain_name] = ProxyFix(hatta.Wiki(config).application)
+
+    if len(splitname) == 2:
+        APPS.setdefault(domain_name, DispatcherMiddleware(default_application, None)).mounts['/' + splitname[1]] = wsgi_app
+    else:
+        APPS[domain_name] = wsgi_app
 
 
 def application(env, start):
     host = env.get('HTTP_HOST', DEFAULT_HOST).split(':')[0].lower()
-    app = APPS.get(host)
+    app = APPS.get(host, default_application)
     return app(env, start)
+
+application = ProxyFix(application, x_prefix=1)
